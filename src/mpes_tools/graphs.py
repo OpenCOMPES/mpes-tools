08120d6919259c856c46e510c731a13b596bb849
Amine Wahada <wahada@FHI.MPG.DE>
2025-04-23 18:53:17 +0200

    added the error bars and checkboxes to show them, added a cursor for the delays and configured the right click button to extract the data

--- src/mpes_tools/graphs.py
+++ src/mpes_tools/graphs.py
@@ -1,14 +1,18 @@
 import sys
 import numpy as np
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QGridLayout,QSlider,QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QGridLayout,QSlider,QLabel,QCheckBox
+from PyQt5.QtCore import Qt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.pyplot as plt
 from IPython.core.getipython import get_ipython
 from mpes_tools.double_click_handler import SubplotClickHandler
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import xarray as xr
+from mpes_tools.right_click_handler import RightClickHandler
+from PyQt5.QtWidgets import QMenu
+from PyQt5.QtGui import QCursor
 class showgraphs(QMainWindow):
-    def __init__(self, x, y_arrays,names,list_axis,list_plot_fits):
+    def __init__(self, x, y_arrays,y_arrays_err,names,list_axis,list_plot_fits):
         super().__init__()
         self.setWindowTitle("Multiple Array Plots")
         self.setGeometry(100, 100, 800, 600)
@@ -17,6 +21,7 @@ class showgraphs(QMainWindow):
         self.dim=x.dims[0]
         self.x = x.data
         self.y_arrays = y_arrays
+        self.y_arrays_err = y_arrays_err
         self.num_plots = len(y_arrays)
         self.list_plot_fits=list_plot_fits 
         self.list_axis=list_axis
@@ -27,34 +32,45 @@ class showgraphs(QMainWindow):
         
         # print(len(x),len(list_plot_fits))
         # print(list_plot_fits[0])
-        slider = QSlider()
-        slider.setOrientation(1)  # 1 = Qt.Horizontal
-        slider.setMinimum(0)
-        slider.setMaximum(len(x)-1)  # Adjust as needed
-        slider.setValue(0)  # Default value
-        slider.valueChanged.connect(self.update_parameter)  # Function to update parameter
+        self.slider = QSlider()
+        self.slider.setOrientation(1)  # 1 = Qt.Horizontal
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(len(x)-1)  # Adjust as needed
+        self.slider.setValue(0)  # Default value
+          # Function to update parameter
         
         self.slider_label = QLabel(f"{x.dims[0]}:0")
         
         self.figure, self.axis = plt.subplots()
         self.canvas = FigureCanvas(self.figure)
         plt.close(self.figure)
+        
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        
+        
+        layout_plot = QVBoxLayout()
+        layout_plot.addWidget(self.toolbar)  # assuming `layout` is your QVBoxLayout or similar
+        layout_plot.addWidget(self.canvas) 
+        
+        widget_plot = QWidget()
+        widget_plot.setLayout(layout_plot)
+        
         vbox = QVBoxLayout()
-        vbox.addWidget(self.canvas)
+        vbox.addWidget(widget_plot)
         vbox.addWidget(self.slider_label)
-        vbox.addWidget(slider)
+        vbox.addWidget(self.slider)
         
         layout.addLayout(vbox, 0, 0)  # Place in top-left
-        self.update_parameter(0)
+        
         self.click_handlers=[]
+        self.handler_list=[]
         self.ax_list=[]
         self.data_list=[]
+        self.cursor_list=[]
         # Create and add buttons and plots for each y array in a 3x3 layout
         for i, y in enumerate(y_arrays):
             # Create a button to show the plot in a new window
-            button = QPushButton(f"Show Plot {i+1}")
-            button.setFixedSize(80, 30)  # Set a fixed size for the button
-            button.clicked.connect(lambda checked, y=y, index=i+1: self.show_plot(y, index, names[i]))
+            
             data_array = xr.DataArray(
                 data=y,
                 dims=[self.dim],                # e.g., 'energy', 'time', etc.
@@ -62,19 +78,58 @@ class showgraphs(QMainWindow):
                 name=names[i]                      # Optional: give it a name (like the plot title)
             )
             self.data_list.append(data_array)
+            
             # Calculate grid position
             row = ((i+1) // 3) * 2   # Each function will take 2 rows: one for the plot, one for the button
             col = (i+1) % 3
-            widget,canvas,ax=self.create_plot_widget(data_array, f"Plot {i+1}_"+names[i])
+            widget,canvas,ax=self.create_plot_widget(data_array,y_arrays_err[i], names[i])
             # Add the plot canvas to the grid
+            checkbox = QCheckBox(f"Show error bars {i+1}")
+            checkbox.setFixedSize(120, 30)  # Adjust size if needed
+            checkbox.stateChanged.connect(lambda state, data_array=data_array, y_err=y_arrays_err[i], index=i: self.show_err(state, data_array, y_err, index))
             
             layout.addWidget(widget, row, col)  # Plot in a 3x3 grid
             # layout.addWidget(self.create_plot_widget(y, f"Plot {i+1}_"+names[i]), row, col)  # Plot in a 3x3 grid
-            layout.addWidget(button, row + 1, col)  # Button directly below the corresponding plot
-            handler = SubplotClickHandler(ax, self.external_callback)
-            canvas.mpl_connect("button_press_event", handler.handle_double_click)
-            self.click_handlers.append(handler)
+            layout.addWidget(checkbox, row + 1, col)  # Button directly below the corresponding plot
+            handler = RightClickHandler(canvas, ax,self.show_pupup_window)
+            canvas.mpl_connect("button_press_event", handler.on_right_click)
+            self.handler_list.append(handler)
+            # handler = SubplotClickHandler(ax, self.external_callback)
+            # canvas.mpl_connect("button_press_event", handler.handle_double_click)
+            # self.click_handlers.append(handler)
             self.ax_list.append(ax)
+            self.cursor=ax.axvline(x=self.x[0], color='r', linestyle='--')
+            self.cursor_list.append(self.cursor)
+        self.update_parameter(0)
+        self.slider.valueChanged.connect(self.update_parameter)
+        
+    def show_pupup_window(self,canvas,ax):
+        # print(f"External callback: clicked subplot ({i},{j})")
+        for i, ax_item in enumerate(self.ax_list):
+            if ax == ax_item:
+                data = self.data_list[i]
+                coords = {k: data.coords[k].values.tolist() for k in data.coords}
+                dims = data.dims
+                name = data.name if data.name else f"data_{i}"
+                menu = QMenu(canvas)
+                action1 = menu.addAction(f"{data.name} plot")
+                action = menu.exec_(QCursor.pos())
+        
+                if action == action1:
+                    print(f'''
+import xarray as xr
+import numpy as np
+
+data_array = xr.DataArray(
+    data=np.array({data.values.tolist()}),
+    dims={dims},
+    coords={coords},
+    name="{name}"
+)
+''')
+                
+
+        
     def external_callback(self,ax):
         # print(f"External callback: clicked subplot ({i},{j})")
         for i, ax_item in enumerate(self.ax_list):
@@ -106,12 +161,15 @@ data_array = xr.DataArray(
         QApplication.processEvents()
         print('results extracted!')
         
-    def create_plot_widget(self, data_array, title):
+    def create_plot_widget(self, data_array, y_err , title):
         """Creates a plot widget for displaying a function."""
         
         figure, ax = plt.subplots()
         plt.close(figure)
-        data_array.plot(ax=ax)
+        
+        # ax.errorbar(data_array[data_array.dims[0]].values, data_array.values, yerr=y_err, fmt='o', capsize=3)
+        ax.plot(data_array[data_array.dims[0]].values, data_array.values,'o')
+        # data_array.plot(ax=ax,fmt='o', capsize=3)
         ax.set_title(title)
         # print('create_plot'+f"self.ax id: {id(ax)}")
         # Create a FigureCanvas to embed in the Qt layout
@@ -127,16 +185,23 @@ data_array = xr.DataArray(
         layout.addWidget(canvas)
         return widget,canvas,ax  # Return the canvas so it can be used in the layout
 
-    def show_plot(self, y, index, name):
-        """Show the plot in a new window."""
-        figure, ax = plt.subplots()
-        ax.plot(self.x, y)
-        ax.set_title(f"Plot {index}_"+ name)
-        ax.grid(True)
-        ax.set_xlabel(self.dim)
-        # ax.set_ylabel('y')
-        plt.show()  # Show the figure in a new window
+    def show_err(self,state,data_array,y_err,i):
+        self.ax_list[i].clear()
+        if state == Qt.Checked:
+            self.ax_list[i].errorbar(data_array[data_array.dims[0]].values, data_array.values, yerr=y_err, fmt='o', capsize=3)
+        else:
+            self.ax_list[i].plot(data_array[data_array.dims[0]].values, data_array.values,'o')
+            # data_array.plot(ax=self.ax_list[i], fmt='o', capsize=3)
+        self.ax_list[i].set_title(data_array.name)
+        self.cursor_list[i]=self.ax_list[i].axvline(x=self.x[self.slider.value()], color='r', linestyle='--')
+        self.ax_list[i].figure.canvas.draw_idle()
+            
     def update_parameter(self, value):
+        for i, c in enumerate(self.cursor_list):
+            if c is not None:
+                c.remove()
+            self.cursor_list[i]=self.ax_list[i].axvline(x=self.x[value], color='r', linestyle='--')
+            self.ax_list[i].figure.canvas.draw_idle()
         base = self.slider_label.text().split(':')[0]
         self.slider_label.setText(f"{base}: {self.x[value]:.2f}")
         self.axis.clear()
@@ -146,19 +211,19 @@ data_array = xr.DataArray(
         self.axis.legend()
         self.figure.tight_layout()
         self.canvas.draw()
-    def create_plot_widget1(self,x_data, y_data, title, return_axes=False):
-        from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-        import matplotlib.pyplot as plt
+    # def create_plot_widget1(self,x_data, y_data, title, return_axes=False):
+    #     from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+    #     import matplotlib.pyplot as plt
     
-        fig, ax = plt.subplots()
-        canvas = FigureCanvas(fig)  
+    #     fig, ax = plt.subplots()
+    #     canvas = FigureCanvas(fig)  
     
-        ax.plot(x_data,y_data)
-        ax.set_title(title)
+    #     ax.plot(x_data,y_data)
+    #     ax.set_title(title)
     
-        if return_axes:
-            return canvas, ax  # Allow updating later
-        return canvas
+    #     if return_axes:
+    #         return canvas, ax  # Allow updating later
+    #     return canvas
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
