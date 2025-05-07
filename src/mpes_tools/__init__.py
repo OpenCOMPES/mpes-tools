1629809a04f95c410c40ddde10916174680842f8
Laurenz Rettig <rettig@fhi-berlin.mpg.de>
2025-05-07 12:16:28 +0200

    update easy access api

--- src/mpes_tools/__init__.py
+++ src/mpes_tools/__init__.py
@@ -1,7 +1,10 @@
 """mpes-tools module easy access APIs."""
 import importlib.metadata
 
+from mpes_tools.fit_panel import fit_panel
+from mpes_tools.Gui_3d import Gui_3d
+from mpes_tools.Main import ARPES_Analyser
 from mpes_tools.show_4d_window import show_4d_window
 
 __version__ = importlib.metadata.version("mpes-tools")
-__all__ = ["show_4d_window"]
+__all__ = ["show_4d_window", "Gui_3d", "fit_panel", "ARPES_Analyser"]
